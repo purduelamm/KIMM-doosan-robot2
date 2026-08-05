da576c0972af7654e5ff0f14f10ebb9031def5fd
dbss1126 <dbss1126@gmail.com>
2026-08-05 23:07:52 +0000

    apply ros connection drop hotfix b43d071

--- dsr_controller2/src/dsr_controller2.cpp
+++ dsr_controller2/src/dsr_controller2.cpp
@@ -1921,7 +1921,7 @@ auto connect_rt_control_cb = [this](const std::shared_ptr<dsr_msgs2::srv::Connec
 auto disconnect_rt_control_cb = [this](const std::shared_ptr<dsr_msgs2::srv::DisconnectRtControl::Request> /*req*/, 
                                        std::shared_ptr<dsr_msgs2::srv::DisconnectRtControl::Response> res) -> void 
 {
-    res->success = Drfl->disconnect_rt_control();
+    // res->success = Drfl->disconnect_rt_control();
 };
 
 auto get_rt_control_output_version_list_cb = [this](const std::shared_ptr<dsr_msgs2::srv::GetRtControlOutputVersionList::Request> /*req*/, 
--- dsr_hardware2/src/dsr_hw_interface2.cpp
+++ dsr_hardware2/src/dsr_hw_interface2.cpp
@@ -721,7 +721,7 @@ return_type DRHWInterface::write(const rclcpp::Time &, const rclcpp::Duration &d
 DRHWInterface::~DRHWInterface()
 {
     Drfl.stop_rt_control();
-    Drfl.disconnect_rt_control();
+    // Drfl.disconnect_rt_control();
     Drfl.close_connection();
 
     RCLCPP_INFO(rclcpp::get_logger("dsr_hw_interface2"),"_______________________________________________\n"); 
